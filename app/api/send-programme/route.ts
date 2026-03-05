cdb01d33e08e0085d382c1b404764d94906a3fac
J7SOO <sofiane.gbkh@gmail.com>
2026-03-05 17:27:22 +0100

    fix stripe url

--- app/api/send-programme/route.ts
+++ app/api/send-programme/route.ts
@@ -2,7 +2,7 @@ import { Resend } from "resend"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export async function POST(req) {
+export async function POST(req: Request) {
 
 const body = await req.json()
 
